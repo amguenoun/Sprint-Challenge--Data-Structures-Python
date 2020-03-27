1dd018c80d2794526e1d48fa1ece85b3451ed8fb
Ahmad Guenoun <amguenoun@gmail.com>
2020-03-27 12:05:33 -0400

    completed names using tree for nlogn runtime

--- names/names.py
+++ names/names.py
@@ -1,4 +1,5 @@
 import time
+from binary_search_tree import BinarySearchTree
 
 start_time = time.time()
 
@@ -12,11 +13,23 @@ f.close()
 
 duplicates = []  # Return the list of duplicates in this data structure
 
-# Replace the nested for loops below with your improvements
-for name_1 in names_1:
-    for name_2 in names_2:
-        if name_1 == name_2:
-            duplicates.append(name_1)
+#      RUNTIME IS O(N^2)
+# Replace the nested for loops below with your improvements 
+# for name_1 in names_1:
+#     for name_2 in names_2:
+#         if name_1 == name_2:
+#             duplicates.append(name_1)
+
+# RUNTIME is O(n log n)
+
+tree = BinarySearchTree(names_1.pop()) #pop last item is constant time
+
+for name in names_1: #loop once is O(n)
+    tree.insert(name)
+
+for name in names_2:  #loop once is O(n)
+    if tree.contains(name): #contains is O(log n)
+        duplicates.append(name)
 
 end_time = time.time()
 print (f"{len(duplicates)} duplicates:\n\n{', '.join(duplicates)}\n\n")
